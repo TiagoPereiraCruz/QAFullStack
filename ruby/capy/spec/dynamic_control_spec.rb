6612dfb40281e0e11a73d94e5001141c395e8e96
Tiago Pereira <tiago.peng@gmail.com>
2019-04-11 22:38:48 -0300

    Atualização de arquivos e Capybara

---
+++ ruby/capy/spec/dynamic_control_spec.rb
@@ -0,0 +1,20 @@
+describe 'Dynamic Control', :dc do
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/dynamic_controls'
+    end
+    it 'quando habilita o campo' do 
+
+        # quando tem a propriedade disabled true o campo está desabilitado
+        # quando não tem a propriedade disable (false) o campo está habilitado
+
+        res = page.has_field? 'movie', disabled: true
+        puts res
+
+        click_button 'Habilita'
+
+        fill_in 'movie', with: 'PASS'
+
+        res = page.has_field? 'movie', disabled: false
+        puts res
+    end
+end
--- ruby/capy/spec/spec_helper.rb
+++ ruby/capy/spec/spec_helper.rb
@@ -19,4 +19,6 @@ end
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
+  Capybara.page.driver.browser.manage.window.maximize
+  config.default_max_wait_time = 5
 end
